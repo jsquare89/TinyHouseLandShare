68afb1a165693301b87c682b25b109cc9bc6c38d
Jarred J <jsquare89@users.noreply.github.com>
2022-03-17 11:28:02 -0700

    Admin controller with dashboard and approve listing routes

---
+++ TinyHouseLandshare/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TinyHouseLandshare.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ApproveListing()
+        {
+            return View();
+        }
+    }
+}
