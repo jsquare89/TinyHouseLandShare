b975690cd3647a7650f3375e6f6d40b886449293
Jarred J <jsquare89@users.noreply.github.com>
2022-10-12 16:52:06 -0700

    user menu working for md+ screens

--- TinyHouseLandshare/wwwroot/js/site.js
+++ TinyHouseLandshare/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+const menuButton = document.getElementById("menu-button")
+const userMenu = document.getElementById("user-menu")
 
-// Write your JavaScript code.
+
+menuButton.onclick = showDropDownMenu;
+
+function showDropDownMenu(e) {
+    console.log("show dropdown menu")
+    menuButton.onclick = function () { };
+    if (e.stopPropagation)
+        e.stopPropagation();   // W3C model
+    else
+        e.cancelBubble = true; // IE model
+
+
+
+
+    userMenu.classList.remove("hidden")
+    document.onclick = function (e) {
+        let element = document.elementFromPoint(e.clientX, e.clientY)
+        if (element == userMenu) {
+            hideDropDownMenu()
+            return
+        }
+        do {
+            if (element == userMenu)
+                return
+        } while (element = element.parentNode)
+        hideDropDownMenu()
+    }
+}
+
+
+
+function hideDropDownMenu() {
+    document.onclick = function () { }
+    userMenu.classList.add("hidden")
+    menuButton.onclick = showDropDownMenu;
+}
